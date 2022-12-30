2586570c66090f6b925b4caa213ea2cabbdd1ebe
Nazaruk-D <nazaruk-dima@mail.ru>
2022-12-30 14:59:30 +0300

    ngFor complex data structure

--- src/app/parents/parents.component.ts
+++ src/app/parents/parents.component.ts
@@ -1,15 +1,63 @@
 import { Component } from '@angular/core'
 
+interface WeekGrade {
+  id: number
+  gradeItem: number
+}
+
+interface Lesson {
+  id: number
+  title: string
+  weekGrades: WeekGrade[]
+}
+
 @Component({
   selector: 'inst-parents',
   templateUrl: './parents.component.html',
   styleUrls: ['./parents.component.scss'],
 })
 export class ParentsComponent {
-  grades: string[] = ['math:5', 'english:3']
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 5,
+        },
+        {
+          id: 1,
+          gradeItem: 5,
+        },
+        {
+          id: 2,
+          gradeItem: 5,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'Physic',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 4,
+        },
+        {
+          id: 1,
+          gradeItem: 5,
+        },
+        {
+          id: 2,
+          gradeItem: 2,
+        },
+      ],
+    },
+  ]
 
   getGrade(grade: string) {
-    this.grades.push(grade)
+    // this.grades.push(grade)
     // this.math = value.math
     // this.phys = value.phys
   }
