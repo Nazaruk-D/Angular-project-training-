db0a44745cd51f2c113d01ea75453efa7d60a61a
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-02 22:47:43 +0300

    NgClass

--- src/app/parents/parents.component.ts
+++ src/app/parents/parents.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core'
 
-interface WeekGrade {
-  id: number
-  gradeItem: number
-}
-
-interface Lesson {
-  id: number
-  title: string
-  weekGrades: WeekGrade[]
-}
-
 @Component({
   selector: 'inst-parents',
   templateUrl: './parents.component.html',
   styleUrls: ['./parents.component.scss'],
 })
 export class ParentsComponent {
-  value = ''
+  isSuccess = true
+
+  constructor() {
+    setTimeout(() => {
+      this.isSuccess = false
+    }, 3000)
+  }
 }
