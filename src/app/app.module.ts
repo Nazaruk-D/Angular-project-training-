bc266a87a65167ffb85438d46c85cbdc247d1a1e
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-07 15:33:07 +0300

    11.2 Srvices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms';
-import { ParentsComponent } from './parents/parents.component';
-import { ChildrenComponent } from './parents/children/children.component'
+import { FormsModule } from '@angular/forms'
+import { ParentsComponent } from './parents/parents.component'
 
 @NgModule({
-  declarations: [AppComponent, ParentsComponent, ChildrenComponent],
+  declarations: [AppComponent, ParentsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/parents/parents.component.ts
+++ src/app/parents/parents.component.ts
@@ -1,29 +1,24 @@
-import { Component } from '@angular/core'
-
-// interface Fruit {
-//   id: string
-//   name: string
-//   price: number
-// }
+import { Component, OnInit } from '@angular/core'
+import { ValueService } from '../services/value.service'
 
 @Component({
   selector: 'inst-parents',
   templateUrl: './parents.component.html',
   styleUrls: ['./parents.component.scss'],
+  providers: [ValueService],
 })
-export class ParentsComponent {
-  title = 'Lorem ipsum dolor sit'
-  url = 'https://samurai.it-incubator.ru/pc/video-content/watch/63493d258e2ab188611d08e6'
-  date = new Date(2023, 1, 6, 14)
-  // fruits: Fruit[] = [
-  //   { id: '1', name: 'apple', price: 10 },
-  //   { id: '2', name: 'orange', price: 20 },
-  //   { id: '3', name: 'watermelon', price: 30 },
-  //   { id: '4', name: 'banana', price: 5 },
-  //   { id: '5', name: 'pears', price: 12 },
-  //   { id: '6', name: 'raspberries', price: 18 },
-  //   { id: '7', name: 'avocados', price: 14 },
-  //   { id: '8', name: 'mangoes', price: 3 },
-  //   { id: '9', name: 'kiwifruit', price: 7 },
-  // ]
+export class ParentsComponent implements OnInit {
+  constructor(private valueService: ValueService) {}
+  value = 0
+  //public test = 0 // test = 0 public ang проставит по умолчанию
+  //private test2 = 0 // не позволит отрисовать, но позволяет работать внктри класса
+  //protected test3 = 0 // внутри класса можно работать, но свойства и методы доступны только наследникам класса
+
+  // getValue() {
+  //   this.test = 2
+  // }
+
+  ngOnInit(): void {
+    this.value = this.valueService.value
+  }
 }
