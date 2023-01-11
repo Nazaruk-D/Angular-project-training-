c25569a30875c14a31a1f1f913d80d70234785a3
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-11 14:58:52 +0300

    12.2 Post и delete request

--- src/app/comp-a/comp-a.component.ts
+++ src/app/comp-a/comp-a.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import { ValueService } from '../services/value.service'
-import { Observable } from 'rxjs'
-import { BeatyLoggerService } from '../services/beaty-logger.service'
 import { HttpClient } from '@angular/common/http'
 
 interface Todo {
@@ -11,6 +8,13 @@ interface Todo {
   title: string
 }
 
+interface BaseResponse<T = {}> {
+  data: T
+  message: string[]
+  fieldsErrors: string[]
+  resultCode: number
+}
+
 @Component({
   selector: 'inst-comp-a',
   templateUrl: './comp-a.component.html',
@@ -18,6 +22,12 @@ interface Todo {
 })
 export class CompAComponent implements OnInit {
   todos: Todo[] = []
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'api-key': '1909a996-577e-417f-8ed9-b0307898e20c',
+    },
+  }
 
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
@@ -26,15 +36,38 @@ export class CompAComponent implements OnInit {
 
   getTodos() {
     this.http
-      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-        withCredentials: true,
-        headers: {
-          'api-key': '1909a996-577e-417f-8ed9-b0307898e20c',
-        },
-      })
+      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', this.httpOptions)
       .subscribe(res => {
         this.todos = res
         console.log(res)
+      })
+  }
+
+  createTodo() {
+    const randomNumber = Math.floor(Math.random() * 100)
+    const title = 'Angular ' + randomNumber
+    this.http
+      .post<BaseResponse<{ item: Todo }>>(
+        'https://social-network.samuraijs.com/api/1.1/todo-lists',
+        { title },
+        this.httpOptions
+      )
+      .subscribe(res => {
+        const newTodo = res.data.item
+        this.todos.unshift(newTodo)
+      })
+  }
+  deleteTodo() {
+    const todoId = '9458254e-592c-44f1-89be-b0350f0b036b'
+    this.http
+      .delete<BaseResponse>(
+        `https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`,
+        this.httpOptions
+      )
+      .subscribe(res => {
+        if (res.resultCode === 0) {
+          this.todos = this.todos.filter(t => t.id !== todoId)
+        }
       })
   }
 }
