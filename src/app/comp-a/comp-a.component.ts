7b1aa99330c697badcb7bda2bc872ac72b20b549
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-13 17:32:00 +0300

    12.6 refactor behaviorSubject, async pype

--- src/app/comp-a/comp-a.component.ts
+++ src/app/comp-a/comp-a.component.ts
@@ -8,8 +8,8 @@ import { Observable, Subscription } from 'rxjs'
   templateUrl: './comp-a.component.html',
   styleUrls: ['./comp-a.component.scss'],
 })
-export class CompAComponent implements OnInit, OnDestroy {
-  todos: Todo[] = []
+export class CompAComponent implements OnInit {
+  todos$!: Observable<Todo[]>
   error = ''
 
   subscriptions: Subscription = new Subscription()
@@ -17,44 +17,21 @@ export class CompAComponent implements OnInit, OnDestroy {
   constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
+    this.todos$ = this.todosService.todos$
     this.getTodos()
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe()
-  }
-
   getTodos() {
-    this.subscriptions.add(
-      this.todosService.getTodos().subscribe({
-        next: (res: Todo[]) => {
-          this.todos = res
-        },
-        error: (err: HttpErrorResponse) => {
-          this.error = err.message
-        },
-      })
-    )
+    this.todosService.getTodos()
   }
 
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100)
     const title = 'Angular ' + randomNumber
-    this.subscriptions.add(
-      this.todosService.createTodo(title).subscribe(res => {
-        const newTodo = res.data.item
-        this.todos.unshift(newTodo)
-      })
-    )
+    this.todosService.createTodo(title)
   }
   deleteTodo() {
-    const todoId = '1ad07d48-7087-4d3d-b884-5b67df9076eb'
-    this.subscriptions.add(
-      this.todosService.deleteTodo(todoId).subscribe(res => {
-        if (res.resultCode === 0) {
-          this.todos = this.todos.filter(t => t.id !== todoId)
-        }
-      })
-    )
+    const todoId = '7abab858-7d27-4f1f-851f-e84e72af52e2'
+    this.todosService.deleteTodo(todoId)
   }
 }
