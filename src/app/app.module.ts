56327e8f119940fcefc5cd9fe3ecbbacea19fd4f
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-14 15:29:24 +0300

    13.1 Forms, Reactive Forms, FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { CompAComponent } from './comp-a/comp-a.component'
 import { HttpClientModule } from '@angular/common/http'
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
-  declarations: [AppComponent, CompAComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, CompAComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms'
+
+@Component({
+  selector: 'inst-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  email = new FormControl('')
+
+  ngOnInit(): void {}
+}
