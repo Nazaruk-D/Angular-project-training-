06d178977155262609dc113185852dc8da440860
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-16 11:40:53 +0300

    14.02 Basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { CompAComponent } from './comp-a/comp-a.component'
 import { HttpClientModule } from '@angular/common/http'
 import { LoginComponent } from './login/login.component'
+import { UsersComponent } from './components/users/users.component'
+import { ProfileComponent } from './components/profile/profile.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-  declarations: [AppComponent, CompAComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [AppComponent, CompAComponent, LoginComponent, UsersComponent, ProfileComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProfileComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'compA', component: CompAComponent },
+      { path: 'users', component: UsersComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
