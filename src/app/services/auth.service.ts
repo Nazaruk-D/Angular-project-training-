1eab883dcbe2c81153f8ada49ab731cf87646b2e
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-19 14:04:16 +0300

    14.11 Guards

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environment/environment'
+
+interface MeResponse {
+  data: {
+    id: number
+    login: string
+    email: string
+  }
+  messages: string[]
+  fieldsErrors: string[]
+  resultCode: number
+}
+
+enum ResultCodes {
+  success = 0,
+  error = 1,
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  isAuth = false
+
+  constructor(private http: HttpClient) {}
+  authMe() {
+    return this.http.get<MeResponse>(`${environment.baseNetworkURL}/auth/me`).subscribe(res => {
+      if (res.resultCode === ResultCodes.success) {
+        this.isAuth = true
+      }
+    })
+  }
+}
